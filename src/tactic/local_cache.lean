4cd64e20628be2a33d138a31dcc5b64ec57d13b7
Keeley Hoek <keeley@hoek.io>
2019-03-20 19:40:46 +1100

    Make it better

--- src/tactic/local_cache.lean
+++ src/tactic/local_cache.lean
@@ -1,44 +1,33 @@
 import tactic.basic
 
-universe u
+namespace name
 
-section lib
-
-def list_to_name_aux : name → list string → name
+private def from_components_aux : name → list string → name
 | n [] := n
-| n (s :: rest) := list_to_name_aux (name.mk_string s n) rest
+| n (s :: rest) := from_components_aux (name.mk_string s n) rest
 
-def list_to_name (l : list string) : name :=
-list_to_name_aux name.anonymous l
+def from_components (l : list string) : name :=
+from_components_aux name.anonymous l
 
-def split_on_aux {α : Type u} [decidable_eq α] (a : α) : list α → list α → list (list α)
-| [] l       := [l.reverse]
-| (h :: t) l := if h = a then
-                  l.reverse :: (split_on_aux t [])
-                else
-                  split_on_aux t (h :: l)
+end name
 
-def list.split_on {α : Type u} [decidable_eq α] (a : α) : list α → list (list α)
-| l := split_on_aux a l []
+namespace tactic
+namespace local_cache
 
-def string.split_on (c : char) (s : string) := (s.to_list.split_on c).map (λ l, l.as_string)
-
-end lib
-
-section local_cache
+section internal
 
 variables {α : Type} [reflected α] [has_reflect α]
 
-meta def mk_full_namespace (ns : name) : name := `data.cache ++ ns
+meta def mk_full_namespace (ns : name) : name := `local_cache ++ ns
 
 meta def get_data_name (ns : name) : tactic name :=
 do o ← tactic.get_options,
    let opt := mk_full_namespace ns,
    match o.get_string opt "" with
-   | "" := do n ← tactic.mk_user_fresh_name,
+   | "" := do n ← mk_user_fresh_name,
               tactic.set_options $ o.set_string opt n.to_string,
               return n
-   | s := return $ list_to_name $ s.split_on '.'
+   | s := return $ name.from_components $ s.split (λ c, c = '.')
    end
 
 meta def save_data (dn : name) (a : α) [reflected a] : tactic unit :=
@@ -49,7 +38,33 @@ do e ← tactic.get_env,
    d ← e.get dn,
    tactic.eval_expr α d.value
 
-meta def calculate_once {α : Type} [reflected α] [has_reflect α] (ns : name) (t : tactic α) : tactic α :=
+end internal
+
+-- Asks whether the namespace `ns` currently has a value-in-cache
+meta def present (ns : name) : tactic bool :=
+do o ← tactic.get_options,
+   match o.get_string (mk_full_namespace ns) "" with
+   | "" := return ff
+   | s  := return tt
+   end
+
+-- Clear cache associated to namespace `ns`
+meta def clear (ns : name) : tactic unit :=
+do o ← tactic.get_options,
+   set_options $ o.set_string (mk_full_namespace ns) ""
+
+end local_cache
+
+open local_cache
+
+-- Using the namespace `ns` as its key, when called for the first
+-- time `calculate_once ns t` runs `t`, saves and returns the result.
+-- Upon subsequent invocations in the same `environment` (usually
+-- just in the same tactic block) return the cached result.
+--
+-- If `α` is just `unit`, this means we just run `t` once each tactic
+-- block.
+meta def run_once {α : Type} [reflected α] [has_reflect α] (ns : name) (t : tactic α) : tactic α :=
 do dn ← get_data_name ns,
    load_data dn <|>
    do {
@@ -58,4 +73,4 @@ do dn ← get_data_name ns,
      return a
    }
 
-end local_cache
+end tactic
